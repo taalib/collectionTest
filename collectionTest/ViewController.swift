6b238ba4b8239ec6e3b5f423ac4b771b23a898ca
Minhas, Taalib (ELS-LON) <minhast@elslabm-156330>
2017-09-15 17:32:16 +0100

    Initial Commit

---
+++ collectionTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  collectionTest
+//
+//  Created by Minhas, Taalib (ELS-LON) on 15/09/2017.
+//  Copyright © 2017 Minhas, Taalib (ELS-LON). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
